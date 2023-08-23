0772d55ea9f2326e3d7bcd1460e3cfd12e3bfb9b
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-08-24 00:14:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = 
+BOT_TOKEN = 2007532918:AAGY19hJZ9voMcUzra2JamqJqs5_wc8Rv3w
+BOT_NAME = вαтмαи
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 6620972
+API_HASH = 3f6835286b03e000ab6d71b37cc35b92
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = 7
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFI("/ !").split()))
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = ("6047510747", "1745047302").split()))
